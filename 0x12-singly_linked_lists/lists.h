2d555ff74db0f5811a9e7f5aee6b6ec2e3ebee94
Silas Osei <silasoxei@gmail.com>
2023-01-05 12:08:22 -0800

    2. Add node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,12 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stddef.h>
+/*
+ * File: lists.h
+*/
+
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -9,7 +14,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *              for Holberton project
  */
 typedef struct list_s
 {
@@ -23,6 +28,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _putchar(char c);
 
-#endif
+#endif /* LISTS_H */
